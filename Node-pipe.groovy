b0e3b222ae6c8dcaa1d7ccc76441be3f6b9bbb93
sivaganesan23 <45412181+sivaganesan23@users.noreply.github.com>
2019-06-21 20:20:48 +0530

    Create Node-pipe.groovy

---
+++ Node-pipe.groovy
@@ -0,0 +1,30 @@
+node('SLAVE2') {
+stage('CLONE') {
+ dir('APPCODE') {
+   git 'https://github.com/opsworkshop/node-hello.git' 
+    }
+   }
+
+ stage('ENV-CREATE') {
+    withCredentials([file(credentialsId: 'CENTOS-USER-PEM-FILE', variable: 'FILE')]) {
+    sh '''cat $FILE >/home/centos/devops.pem
+      chmod 600 /home/centos/devops.pem
+     '''
+    }
+   dir('TERRAFORM') {
+     git 'https://github.com/sivaganesan23/nodejs-proj.git'
+      wrap([$class: 'AnsiColorBuildWrapper', 'colorMapName': 'xterm']) {
+      withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'AWS-USER-KEYS', usernameVariable: 'ACCESS_KEY', passwordVariable: 'SECRET_KEY']]) {
+      sh '''
+      export AWS_ACCESS_KEY_ID=$ACCESS_KEY
+      export AWS_SECRET_ACCESS_KEY=$SECRET_KEY
+      export AWS_DEFAULT_REGION=us-east-2     
+      cd terraform
+      terraform init
+      terraform apply -auto-approve
+      '''
+              }
+          }
+          }
+ }
+}
